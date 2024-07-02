77ab871280c5b6222e45eafff634343fecb025f1
Kevin Rosario <29806319+KevinRosario01@users.noreply.github.com>
2024-07-02 00:56:30 +0000

    Added mobile response to navbar

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -8,7 +8,7 @@ export function Navbar ()  {
           
             <a href="#" className="text-white text-lg font-bold">Start Tailwinds</a>
         </div>
-        <div className="flex space-x-4">
+        <div className="invisible lg:visible flex space-x-4">
             <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
             <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
             <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Services</a>
